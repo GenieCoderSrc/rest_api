d058127f677482f1c04ac9b54e3a88b5bbb4b327
SuraiyaKhatunItHome <suraiya.it.home@gmail.com>
2025-03-30 13:28:22 +0600

    first commit

---
+++ lib/config/di/rest_api_service_get_it_register.dart
@@ -0,0 +1,32 @@
+import 'package:global/get_it_di.dart';
+import 'package:http/http.dart' as http;
+import 'package:rest_api/rest_api.dart';
+
+void restApiServiceGetItRegister() {
+  /// ----------------- Rest API Data Service--------------
+  ///  Service
+  sl.registerLazySingleton<IResponseHandler<http.Response>>(
+      () => ResponseHandlerHttpImpl());
+
+  sl.registerLazySingleton<IRestApiConfig>(() => RestApiConfigAppDevModeImpl());
+  sl.registerLazySingleton<IHeaderProvider>(() => HeaderProviderBearerImpl());
+  sl.registerLazySingleton<IUrlGenerator>(() => UrlGeneratorRestApiImpl());
+  // sl.registerLazySingleton<IParseHttpResponseData>(
+  //     () => ParseHttpResponseData());
+
+  // sl.registerLazySingleton<IRestApiDataService>(() =>
+  //     RestApiDataServiceDioImpl(dio: sl(), iApiResponseHandlerService: sl()));
+
+  sl.registerLazySingleton<IRestApiDataSender>(() => RestApiDataSenderHttpImpl(
+        client: sl(),
+        iResponseHandler: sl(),
+        iRestApiConfig: sl(),
+        iRestApiHeaderProvider: sl(),
+        iApiPathUrlGenerator: sl(),
+      ));
+
+  sl.registerLazySingleton<IRestApiCrudService>(
+      () => RestApiCrudServiceRestApiDataSenderImpl(iRestApiDataSender: sl()));
+
+  sl.registerLazySingleton(() => http.Client());
+}
